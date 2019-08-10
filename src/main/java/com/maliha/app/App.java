57fced080805e543aec5d9094609613bec63d40a
Maliha Mowla <sincerelymaliha@gmail.com>
2019-08-10 13:38:45 -0400

    added pom.xml, app, apptest

---
+++ src/main/java/com/maliha/app/App.java
@@ -0,0 +1,13 @@
+package com.maliha.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
